6f51399f268d0fe854140fb8d946aad758cacf80
jafarvandsirvan <jafarvandsirvan@gmail.com>
2025-03-06 16:56:04 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return 'Bot is running!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000))  # پورت از محیطی که Render تنظیم کرده می‌خونه
+    app.run(host='0.0.0.0', port=port)
